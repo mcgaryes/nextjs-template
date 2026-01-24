e8cb3cb76f7d4c30050c14448948a651a88ddc06
Eric McGary <mcgaryes@gmail.com>
2026-01-24 14:03:02 -0500

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import { Providers } from '@/app/providers';
 import './globals.css';
 
 const geistSans = Geist({
@@ -25,9 +24,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <Providers>{children}</Providers>
-            </body>
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
         </html>
     );
 }
