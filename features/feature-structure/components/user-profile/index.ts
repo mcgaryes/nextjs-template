d9ac3e8b35927d4e67cb5d8c0bc88a634f419c5c
Eric McGary <mcgaryes@gmail.com>
2026-02-01 11:34:29 -0500

    removing nextjs-template

--- features/feature-structure/components/user-profile/index.ts
+++
@@ -1,5 +0,0 @@
-export { UserProfile } from "./user-profile";
-export { UserProfileLoading } from "./user-profile-loading";
-export { UserProfileEmpty } from "./user-profile-empty";
-export { UserProfileErrored } from "./user-profile-errored";
-export { UserProfileView } from "./user-profile-view";
